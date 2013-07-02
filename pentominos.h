309d1ff4af39eb1aa652a69167e476050d746bd2
Dave Compton <to.dave.c@gmail.com>
2013-07-02 10:31:55 -0700

    removed penta project from penta solution.

--- pentominos.h
+++
@@ -1,8 +0,0 @@
-#ifndef pentominos_h__
-#define pentominos_h__
-
-#include "BoolPic.h"
-
-const unsigned int PIECE_COUNT=12;
-extern const Pentomino pentominos[PIECE_COUNT];
-#endif //  pentominos_h__
