313f46c50188fc4637144f71fbb51b6a13e59974
Dave Compton <to.dave.c@gmail.com>
2013-07-05 17:02:47 -0700

    adding swig and c++ to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup, Extension
 setup(name='exactcover',
       version='1.0',
       ext_modules=[
-          Extension('_exactcover', ['exactcover.i'], swig_opts=['-c++', '-python'])],
+          Extension('_exactcover', ['Coverings.cxx', 'exactcover.i'], extra_compile_args=['-std=c++11'], swig_opts=['-c++'])],
       )
 
