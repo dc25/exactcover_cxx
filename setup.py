268f075021c86d6f10b0a39047dc9456952eb3c3
Dave Compton <to.dave.c@gmail.com>
2013-07-05 18:14:04 -0700

    minor formatting cleanup

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from distutils.core import setup, Extension
 
 setup(name='exactcover',
       version='1.0',
+      py_modules=['exactcover'],
       ext_modules=[
-          Extension('_exactcover', ['Coverings.cxx', 'exactcover.i'], extra_compile_args=['-std=c++11'], swig_opts=['-c++'])],
-      )
+          Extension('_exactcover', 
+              ['Coverings.cxx', 'exactcover.i'], 
+              extra_compile_args=['-std=c++11'], 
+              swig_opts=['-c++'])],
+     )
 
