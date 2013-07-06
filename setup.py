648f177a9dcc599408bec8b1fa44a92b3cd17d04
Dave Compton <to.dave.c@gmail.com>
2013-07-06 08:42:01 -0700

    filling out setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from distutils.core import setup, Extension
 
 setup(name='exactcover_cxx',
       version='1.0',
+      description='Exact Cover / Dancing Links solver',
+      author='Dave Compton',
+      author_email='davecompton200@gmail.com',
       py_modules=['exactcover_cxx'],
       ext_modules=[
           Extension('_exactcover_cxx', 
