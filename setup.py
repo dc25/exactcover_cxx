41ea8e40c5c0e510458051447428b02d5951ffd3
Dave Compton <to.dave.c@gmail.com>
2013-07-05 18:43:29 -0700

    renamed exactcover to exactcover_cxx

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup, Extension
 
-setup(name='exactcover',
+setup(name='exactcover_cxx',
       version='1.0',
-      py_modules=['exactcover'],
+      py_modules=['exactcover_cxx'],
       ext_modules=[
-          Extension('_exactcover', 
-              ['Coverings.cxx', 'exactcover.i'], 
+          Extension('_exactcover_cxx', 
+              ['Coverings.cxx', 'exactcover_cxx.i'], 
               extra_compile_args=['-std=c++11'], 
               swig_opts=['-c++'])],
      )
