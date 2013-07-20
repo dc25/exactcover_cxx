71d0f4e4efa3cbaf3669a92aad9abef56b9266d1
Dave Compton <to.dave.c@gmail.com>
2013-07-20 14:13:37 -0700

    broke Cell class into its own file which is accessed from Answers.cxx and Coverings.cxx

---
+++ Cell.h
@@ -0,0 +1,27 @@
+#ifndef CELL_H__
+#define CELL_H__
+
+class Cell {
+public:
+    Cell()
+        : m_left(0), m_right(0), m_up(0), m_down(0), m_col(0), m_useCount(0), m_name(0)
+    {
+    }
+
+    ~Cell()
+    {
+        delete[] m_name;
+    }
+
+    Cell* m_left;
+    Cell* m_right;
+    Cell* m_up;
+    Cell* m_down;
+    Cell* m_col;
+
+	unsigned int m_useCount;
+
+    char* m_name;  // tried using a string here but that slowed down run time noticably.
+};
+
+#endif // CELL_H__
