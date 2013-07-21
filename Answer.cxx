479800531389a7229e5abe9e3a7942290810f897
Dave Compton <to.dave.c@gmail.com>
2013-07-21 07:46:58 -0700

    Added comments; removed unused resize function.

--- Answer.cxx
+++ Answer.cxx
@@ -1,16 +1,44 @@
+/*
+Dancing Links / Exact Cover library with c++ and python interfaces
+Copyright (c) 2013 by David Compton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 #include "Answer.h"
 #include <algorithm>
 
 using namespace std;
 
-Answer::Answer(const std::vector< std::vector<std::string> >& vvs)
+// 
+// Initialize an Answer from a vector of vectors of strings.
+// The contents of the Answer are the same as the "vvs" argument 
+// except that the outer vector becomes a vector of pointers to 
+// vectors of strings and the strings in the inner vector are sorted.
+//
+Answer::Answer(const vector< vector<string> >& vvs)
 {
-    for (size_t i = 0; i < vvs.size(); ++i)
+    for (auto vs : vvs)
     {
-        m_nameSolution.push_back(make_shared< vector <string> >(vvs[i]));
-        std::vector<std::string>& row =  *(m_nameSolution[i]);
-        // name vectors are sorted for vector equality testing.
-        sort(row.begin(), row.end());
+        auto row = make_shared< vector <string> >(vs);
+        sort(row->begin(), row->end()); // for vector equality testing.
+        m_nameSolution.push_back(row);
     }
 }
 
